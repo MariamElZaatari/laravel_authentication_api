93c6b49259d3eb3e57332eac5c4261b06abf7b07
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-03 11:32:55 +0200

    UserController Clean and Comment

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,10 +8,12 @@ use Validator;
 
 class UserController extends Controller
 {
+    // --------- Update Method -----------
     public function update(Request $request)
     {
-
+        // Check if user is authenticated to update info
         if (auth()->user()->id == $request->id) {
+            // Validate Request
             $validator = Validator::make($request->all(), [
                 'id' => 'required|integer',
                 'first_name' => 'required|string|alpha|between:2,100',
@@ -21,42 +23,55 @@ class UserController extends Controller
                 'age' => 'required|numeric|integer|min:12|max:99',
             ]);
 
+            // If validator fails, json encode error with status
             if ($validator->fails()) {
                 return response()->json($validator->errors()->toJson(), 400);
             }
 
+            // Update user info where id=request.id
             User::where('id', $request->id)
                 ->update($validator->validated());
 
+            // Select * from user where id=request.id
             $user = User::find($request->id);
 
+            // Return user along with success message and status
             return response()->json([
                 'message' => 'User Successfully Updated',
                 'user' => $user,
             ], 202);
         }
 
+        // if user not authenticated to update
         return response()->json([
             'message' => 'Not Authorized',
         ], 401);
 
     }
-
+    // -----------------------------------
+    
+    // --------- Delete Method -----------
     public function delete($id)
     {
+        // Check if user is authenticated to delete account
         if (auth()->user()->id == $id) {
+            
+            //Select * from user where id=$id
             $user = User::find((int) $id);
-
+            
             $user->delete();
-
+            
+            // Return user along with success message and status
             return response()->json([
                 'message' => 'User Successfully Deleted',
                 'user' => $user,
             ], 202);
         }
 
+        // if user not authenticated to delete
         return response()->json([
             'message' => 'Not Authorized',
         ], 401);
     }
+    // -----------------------------------
 }
