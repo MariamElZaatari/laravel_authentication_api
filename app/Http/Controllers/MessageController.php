d2b6c24a9feb6dea7d6d7b0873f4cd83a056a742
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-03 11:32:34 +0200

    MessageController Clean and Comment

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -8,25 +8,31 @@ use Validator;
 
 class MessageController extends Controller
 {
+
+    // -------- Create Message Method --------
     public function create(Request $request)
     {
-
+        // Validate Request
         $validator = Validator::make($request->all(), [
             'email' => 'required|string|email|max:50',
             'subject' => 'required|string|max:20',
             'text' => 'required|string|between:3,500',
         ]);
 
+        // If validator fails, json encode error with status
         if ($validator->fails()) {
             return response()->json($validator->errors()->toJson(), 400);
         }
 
+        // if validated, create message
         $message = Message::create($validator->validated());
 
+        // Return message along with success message and status
         return response()->json([
             'message' => 'Message successfully added',
             'message_info' => $message,
         ], 201);
-
     }
+    // ---------------------------------------
+    
 }
